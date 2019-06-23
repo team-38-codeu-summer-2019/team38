acd84c17bf5a250426a75be26bec947c4621a5f5
rahmaniaam <rahmaniaam26@gmail.com>
2019-06-23 20:27:06 +0700

    Aci images2 (#26)
    * Created home servlet
    
    * created form handler servlet
    
    * created blobstore upload servlet
    
    * add html for blobstore example
    
    * seperate js from blobstore html
    
    * Fixed incorrect package imports

--- src/main/java/com/google/codeu/servlets/FormHandlerServlet.java
+++ src/main/java/com/google/codeu/servlets/FormHandlerServlet.java
@@ -11,9 +11,11 @@ import javax.servlet.ServletOutputStream;
 import com.google.appengine.api.blobstore.BlobstoreService;
 import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
 import com.google.appengine.api.blobstore.BlobInfo;
+import com.google.appengine.api.blobstore.BlobInfoFactory;
 import com.google.appengine.api.blobstore.BlobKey;
-import com.google.appengine.api.blobstore.ImagesService;
-import com.google.appengine.api.blobstore.ServingUrlOptions;
+import com.google.appengine.api.images.ImagesService;
+import com.google.appengine.api.images.ImagesServiceFactory;
+import com.google.appengine.api.images.ServingUrlOptions;
 
 @WebServlet("/my-form-handler")
 public class FormHandlerServlet extends HttpServlet {
@@ -39,29 +41,53 @@ public class FormHandlerServlet extends HttpServlet {
   }
 
   /**
+<<<<<<< HEAD
+   * Returns a URL that points to the uploaded file, or null if the user didn't
+   * upload a file.
+   */
+  private String getUploadedFileUrl(HttpServletRequest request, String formInputElementName) {
+=======
    * Returns a URL that points to the uploaded file, or null if the user didn't upload a file.
    */
+
   private String getUploadedFileUrl(HttpServletRequest request, String formInputElementName){
+>>>>>>> 0296324df0e361f1f1d3f0e4110b944e8c5dcd40
     BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
     Map<String, List<BlobKey>> blobs = blobstoreService.getUploads(request);
     List<BlobKey> blobKeys = blobs.get("image");
 
+<<<<<<< HEAD
+    // User submitted form without selecting a file, so we can't get a URL.
+    // (devserver)
+    if (blobKeys == null || blobKeys.isEmpty()) {
+=======
     // User submitted form without selecting a file, so we can't get a URL. (devserver)
     if(blobKeys == null || blobKeys.isEmpty()) {
+>>>>>>> 0296324df0e361f1f1d3f0e4110b944e8c5dcd40
       return null;
     }
 
     // Our form only contains a single file input, so get the first index.
     BlobKey blobKey = blobKeys.get(0);
 
+<<<<<<< HEAD
+    // User submitted form without selecting a file, so we can't get a URL. (live
+    // server)
+=======
     // User submitted form without selecting a file, so we can't get a URL. (live server)
+>>>>>>> 0296324df0e361f1f1d3f0e4110b944e8c5dcd40
     BlobInfo blobInfo = new BlobInfoFactory().loadBlobInfo(blobKey);
     if (blobInfo.getSize() == 0) {
       blobstoreService.delete(blobKey);
       return null;
     }
 
+<<<<<<< HEAD
+    // We could check the validity of the file here, e.g. to make sure it's an image
+    // file
+=======
     // We could check the validity of the file here, e.g. to make sure it's an image file
+>>>>>>> 0296324df0e361f1f1d3f0e4110b944e8c5dcd40
     // https://stackoverflow.com/q/10779564/873165
 
     // Use ImagesService to get a URL that points to the uploaded file.
