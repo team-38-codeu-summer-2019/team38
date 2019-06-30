12ff41cfc0b7934918b721dfc52cd59c9e15a685
lungsin <kweelungsin@gmail.com>
2019-07-01 00:06:22 +0700

    WIP review front end

---
+++ src/main/webapp/js/review-loader.js
@@ -0,0 +1,91 @@
+
+
+// Get ?merchant=XYZ parameter value
+const urlParams = new URLSearchParams(window.location.search);
+const parameterMerchant = urlParams.get('merchant');
+
+// URL must include ?merchant=XYZ parameter. If not, redirect to homepage.
+if (!parameterMerchant) {
+  window.location.replace('/');
+}
+
+/** Sets the page title based on the URL parameter username. */
+function setPageTitle() {
+  document.getElementById('page-title').innerText = parameterMerchant;
+  document.title = parameterMerchant + ' - Review';
+}
+
+
+/** Fetches messages and add them to the page. */
+function fetchReviews() {
+  const url = '/reviews?merchant=' + parameterMerchant;
+  fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((reviews) => {
+        const reviewsContainer = document.getElementById('reviews-container');
+        if (reviews.length == 0) {
+          reviewsContainer.innerHTML = '<p>This merchant has no reviews yet.</p>';
+        } else {
+          reviewsContainer.innerHTML = '';
+        }
+        reviews.forEach((review) => {
+          const reviewDiv = buildMessageDiv(review);
+          reviewsContainer.appendChild(reviewDiv);
+        });
+
+        setOverallRating(reviews);
+      });
+}
+
+/** Calculate average rating from all reviews.
+  * @param {Array of Reviews} reviews
+  * @return {Float} avg rating
+  */
+function calculateAvgRating(reviews) {
+    avg = 0.0;
+    reviews.forEach((review) => {
+        avg += review.rating;
+    });
+    avg /= reviews.length;
+    return avg;
+}
+
+/** Set the overall rating from a particular merchant.
+  * @param {Array of Reviews} reviews
+  */
+function setOverallRating(reviews) {
+    rating = calculateAvgRating(reviews);
+    const ratingContainer = document.getElementById('overall-rating-container');
+    ratingContainer.innerHTML = '<p>Rating = ${rating}</p>';
+}
+
+/**
+ * Builds an element that displays the review.
+ * @param {Review} review
+ * @return {Element}
+ */
+function buildReviewDiv(review) {
+  const headerDiv = document.createElement('div');
+  headerDiv.classList.add('review-header');
+  headerDiv.appendChild(document.createTextNode(
+      review.user + ' - ' + new Date(review.timestamp)));
+
+  const bodyDiv = document.createElement('div');
+  bodyDiv.classList.add('review-body');
+  bodyDiv.innerHTML = review.text;
+
+  const reviewDiv = document.createElement('div');
+  reviewDiv.classList.add('review-div');
+  reviewDiv.appendChild(headerDiv);
+  reviewDiv.appendChild(bodyDiv);
+
+  return reviewDiv;
+}
+
+/** Fetches data and populates the UI of the page. */
+function buildUI() {
+  setPageTitle();
+  fetchReviews();
+}
