2bda434430f16fea227f7065ec3a300bc07e333b
Felicia Chen <felichn@gmail.com>
2019-07-15 17:39:03 +0800

    Calculate distance from longitude and longitude of merchant and user's current location

--- src/main/java/com/google/codeu/servlets/SearchServlet.java
+++ src/main/java/com/google/codeu/servlets/SearchServlet.java
@@ -19,33 +19,10 @@ import org.jsoup.safety.Whitelist;
  */
 @WebServlet("/search")
 public class SearchServlet extends HttpServlet{
-  
-//  private Datastore datastore;
- 
 
- @Override
- public void init() {
-//   datastore = new Datastore();
-    // datastore = [{"id":"123123","cafe":"McDonald's","name":"McDonald's","location":"2/F ABC Building, Centennial Campus","distance_meters":50,"cuisine":"Western"},
-    //             {"id":"789789","cafe":"Swire Canteen","name":"Ebeneezer's","location":"5/F Computing Building, Main Campus","distance_meters":100,"cuisine":"Indian"}];
- }
- 
- /**
-  * Responds with a JSON representation of Message data for all users.
-  */
- @Override
- public void doGet(HttpServletRequest request, HttpServletResponse response)
+ public void doPost(HttpServletRequest request, HttpServletResponse response)
    throws IOException {
-
-//   response.setContentType("application/json");
-
   String merchant = Jsoup.clean(request.getParameter("merchant"), Whitelist.none());
-  
-//   List<Message> messages = datastore.getAllMessages();
-//   Gson gson = new Gson();
-//   String json = gson.toJson(messages);
-  
-//   response.getOutputStream().println(json);
   response.sendRedirect("/search.html?merchant=" + merchant);
  }
 }
