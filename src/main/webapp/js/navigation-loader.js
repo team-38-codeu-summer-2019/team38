bcaec6b191ead0b75df1e1db2bcdea79e428f35f
lungsin <kweelungsin@gmail.com>
2019-07-19 08:22:39 +0700

    integrate all pages to navbar

--- src/main/webapp/js/navigation-loader.js
+++ src/main/webapp/js/navigation-loader.js
@@ -31,8 +31,8 @@ function addLoginOrLogoutLinkToNavigation() {
       })
       .then((loginStatus) => {
         if (loginStatus.isLoggedIn) {
-          //navigationElement.appendChild(createListItem(createLink(
-          //    '/user-page.html?user=' + loginStatus.username, 'Your Page')));
+          navigationElement.appendChild(createListItem(createLink(
+              '/user-page.html?user=' + loginStatus.username, 'My Profile')));
 
           navigationElement.appendChild(
               createListItem(createLink('/logout', 'Logout')));
